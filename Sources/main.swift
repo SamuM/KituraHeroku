214c561d3b1ec17a017679cc029bac5842d5e17b
Samu Mukkala <samu.mukkala@futurice.com>
2017-08-23 11:36:55 +0300

    added select query

--- Sources/main.swift
+++ Sources/main.swift
@@ -27,7 +27,7 @@ if let requestedHost = ProcessInfo.processInfo.environment["DATABASE_URL"] {
     // There is an annoying bug in Kitura that requires us to make the postgress address coming from Heroku to have first letter as uppercase
     var urlComp = URLComponents(string: requestedHost)
     urlComp?.scheme = "Postgres"
-    Log.verbose(requestedHost)
+
     if let url = urlComp?.url {
         DBHost = url
     } else {
@@ -37,10 +37,11 @@ if let requestedHost = ProcessInfo.processInfo.environment["DATABASE_URL"] {
 } else {
     DBHost = URL(string: defaultDBHost)!
 }
+
 Log.verbose("\(DBHost.absoluteURL)")
 let connection = PostgreSQLConnection(url: DBHost)
 
-// Handle HTTP GET requests to /
+// Handle HTTP GET requests to root
 router.get("/") {
     request, response, next in
     response.send("Hello, World!")
@@ -49,38 +50,70 @@ router.get("/") {
 
 router.get("/addchicken/:name/:destiny") {
     request, response, next in
-        connection.connect { error in
-            if let error = error {
-                Log.verbose("Connection error: \(error)")
 
-            } else {
-                guard let name = request.parameters["name"],
-                    let destiny = request.parameters["destiny"] else {
-                        
-                        Log.error("No parameters found")
-                        return
-                }
-                let chickentable = ChickenTable()
-                
-                let insertQuery = Insert(into: chickentable, valueTuples: (chickentable.name, name), (chickentable.destiny, destiny))
-                
-                connection.execute(query: insertQuery, onCompletion: { result in
-                    if result.success {
-                        response.send("We are DONE! \(result.asValue ?? "no value")")
-                    }
-                    Log.verbose("\(String(describing: result.asError))")
-                    next()
-                })
-
+    // To properly handle DB connections you should create an connection pool for it. https://github.com/IBM-Swift/Swift-Kuery
+    connection.connect { error in
+        if let error = error {
+            Log.error("Connection error: \(error)")
+            next()
+        } else {
+            guard let name = request.parameters["name"],
+                let destiny = request.parameters["destiny"] else {
+                    
+                Log.error("No parameters found")
+                return
             }
+
+            let chickentable = ChickenTable()
+
+            // Create an InsertQuery and add values to the fields using Tuples
+            let insertQuery = Insert(into: chickentable, valueTuples: (chickentable.name, name), (chickentable.destiny, destiny))
+
+            connection.execute(query: insertQuery, onCompletion: { result in
+                if result.success {
+                    response.send("Data added!")
+                }
+                response.send("Error: \(String(describing: result.asError))")
+                Log.error("\(String(describing: result.asError))")
+                next()
+            })
+
         }
+        // Close connection to DB since we are not using connection pooling
+        connection.closeConnection()
+    }
 
-    
-
-    //response.send("Hi! My name is \(name) and when I grow up I'm going to be an \(destiny)!")
-   // next()
 }
 
+router.get("/chickens") {
+    request, response, next in
+    
+    connection.connect { error in
+        if let error = error {
+            Log.error("Connection error: \(error)")
+            next()
+        } else {
+            
+            let chickentable = ChickenTable()
+            
+            // Create an InsertQuery and add values to the fields using Tuples
+            let selectQuery = Select([chickentable.name, chickentable.destiny], from: chickentable)
+            
+            connection.execute(query: selectQuery, onCompletion: { result in
+                if result.success {
+                    response.send("Here is the data: \(result.asValue ?? "no asValue") \n \(result.asResultSet ?? "no asResultSet") \n \(result.asRows ?? "no asRows")")
+                }
+                response.send("Error: \(String(describing: result.asError))")
+                Log.error("\(String(describing: result.asError))")
+                next()
+            })
+            
+        }
+        // Close connection to DB since we are not using connection pooling
+        connection.closeConnection()
+    }
+    
+}
 
 // Define app to use different port when used from Heroku.
 
